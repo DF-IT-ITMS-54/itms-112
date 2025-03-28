30d0f89bedde20e1b0486fe904d71ea9bd991bc6
Tzuyang-Dafon <tzuyang.tsai@df-recycle.com>
2025-03-28 06:04:38 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ITMS-112',
+  webDir: 'dist'
+};
+
+export default config;
